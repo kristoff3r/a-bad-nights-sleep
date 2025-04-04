0550690ff5fbefc30c41731f405f1333264d08d4
Kristoffer Søholm <k.soeholm@gmail.com>
2025-04-04 19:53:49 +0200

    Initial commit

---
+++ src/character.rs
@@ -0,0 +1,141 @@
+use avian2d::{math::*, prelude::*};
+use bevy::prelude::*;
+
+pub struct CharacterControllerPlugin;
+
+impl Plugin for CharacterControllerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<MovementAction>().add_systems(
+            Update,
+            (keyboard_input, movement, apply_movement_damping).chain(),
+        );
+    }
+}
+
+/// An event sent for a movement input action.
+#[derive(Event)]
+pub enum MovementAction {
+    Move(Vector2),
+}
+
+/// A marker component indicating that an entity is using a character controller.
+#[derive(Component)]
+#[require(Transform)]
+pub struct CharacterController;
+
+/// The acceleration used for character movement.
+#[derive(Component)]
+pub struct MovementAcceleration(Scalar);
+
+/// The damping factor used for slowing down movement.
+#[derive(Component)]
+pub struct MovementDampingFactor(Scalar);
+
+/// A bundle that contains the components needed for a basic
+/// kinematic character controller.
+#[derive(Bundle)]
+pub struct CharacterControllerBundle {
+    character_controller: CharacterController,
+    rigid_body: RigidBody,
+    collider: Collider,
+    ground_caster: ShapeCaster,
+    movement: MovementBundle,
+}
+
+/// A bundle that contains components for character movement.
+#[derive(Bundle)]
+pub struct MovementBundle {
+    acceleration: MovementAcceleration,
+    damping: MovementDampingFactor,
+}
+
+impl MovementBundle {
+    pub const fn new(acceleration: Scalar, damping: Scalar) -> Self {
+        Self {
+            acceleration: MovementAcceleration(acceleration),
+            damping: MovementDampingFactor(damping),
+        }
+    }
+}
+
+impl Default for MovementBundle {
+    fn default() -> Self {
+        Self::new(30.0, 0.9)
+    }
+}
+
+impl CharacterControllerBundle {
+    pub fn new(collider: Collider) -> Self {
+        // Create shape caster as a slightly smaller version of collider
+        let mut caster_shape = collider.clone();
+        caster_shape.set_scale(Vector::ONE * 0.99, 10);
+
+        Self {
+            character_controller: CharacterController,
+            rigid_body: RigidBody::Kinematic,
+            collider,
+            ground_caster: ShapeCaster::new(caster_shape, Vector::ZERO, 0.0, Dir2::NEG_Y)
+                .with_max_distance(10.0),
+            movement: MovementBundle::default(),
+        }
+    }
+
+    pub fn with_movement(mut self, acceleration: Scalar, damping: Scalar) -> Self {
+        self.movement = MovementBundle::new(acceleration, damping);
+        self
+    }
+}
+
+/// Sends [`MovementAction`] events based on keyboard input.
+fn keyboard_input(
+    mut movement_event_writer: EventWriter<MovementAction>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let left = keyboard_input.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]);
+    let right = keyboard_input.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]);
+    let up = keyboard_input.any_pressed([KeyCode::KeyW, KeyCode::ArrowUp]);
+    let down = keyboard_input.any_pressed([KeyCode::KeyS, KeyCode::ArrowDown]);
+
+    let horizontal = right as i8 - left as i8;
+    let horizontal = horizontal as Scalar;
+    let vertical = up as i8 - down as i8;
+    let vertical = vertical as Scalar;
+
+    if horizontal != 0.0 || vertical != 0.0 {
+        movement_event_writer.send(MovementAction::Move(Vector2::new(horizontal, vertical)));
+    }
+}
+
+/// Responds to [`MovementAction`] events and moves character controllers accordingly.
+fn movement(
+    time: Res<Time>,
+    mut movement_event_reader: EventReader<MovementAction>,
+    mut controllers: Query<(&MovementAcceleration, &mut LinearVelocity)>,
+) {
+    // Precision is adjusted so that the example works with
+    // both the `f32` and `f64` features. Otherwise you don't need this.
+    let delta_time = time.delta_secs_f64().adjust_precision();
+
+    for event in movement_event_reader.read() {
+        for (movement_acceleration, mut linear_velocity) in &mut controllers {
+            match event {
+                MovementAction::Move(direction) => {
+                    if direction.x != 0.0 {
+                        linear_velocity.x += direction.x * movement_acceleration.0 * delta_time;
+                    }
+                    if direction.y != 0.0 {
+                        linear_velocity.y += direction.y * movement_acceleration.0 * delta_time;
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// Slows down movement in the X direction.
+fn apply_movement_damping(mut query: Query<(&MovementDampingFactor, &mut LinearVelocity)>) {
+    for (damping_factor, mut linear_velocity) in &mut query {
+        // We could use `LinearDamping`, but we don't want to dampen movement along the Y axis
+        linear_velocity.x *= damping_factor.0;
+    }
+}
